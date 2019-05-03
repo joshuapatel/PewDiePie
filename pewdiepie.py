3628b23d0560ec3193fe4245256abf78bb033b49
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-05-03 18:26:51 -0400

    Added support for Redis

--- pewdiepie.py
+++ pewdiepie.py
@@ -1,9 +1,15 @@
+# -> Discord
 import discord
 from discord.ext import commands
+# -> Configuration
 import config
-from random import randint
+# -> Miscellaneous
+import random
 import datetime
+# -> Database and cache
 import asyncpg
+import aioredis
+# -> Loop
 import asyncio
 import sys
 import os
@@ -18,7 +24,7 @@ async def custom_prefix(bot, message):
     try:
         prefix = bot.prefixes.get(message.guild.id)
     except AttributeError:
-        rnd = randint(12**2, 12**4)
+        rnd = random.randint(12**2, 12**4)
         return str(rnd)
 
     if prefix == None:
@@ -39,38 +45,41 @@ class PewDiePie(commands.AutoShardedBot):
             case_insensitive = True,
             max_messages = 500,
             fetch_offline_members = False,
-            reconnect = True
+            reconnect = True,
+            owner_id = 498678645716418578
         )
+        self.prefixes = {}
+        self.pool = None
+        self.redis = None
 
     async def on_ready(self):
         if not hasattr(self, "uptime"):
             self.uptime = datetime.datetime.utcnow()
-        
+
         print(f"{self.user.name} is ready!")
 
     async def on_connect(self):
-        if not hasattr(self, "pool"):
-            pool_creds = {
-                "user": config.db_user,
-                "password": config.db_password,
-                "port": 5432,
-                "host": "localhost",
-                "database": "tseries"
-            }
+        pool_creds = {
+            "user": config.db_user,
+            "password": config.db_password,
+            "port": 5432,
+            "host": "localhost",
+            "database": "tseries"
+        }
 
-            try:
-                self.pool = await asyncpg.create_pool(**pool_creds)
-            except Exception as error:
-                print("There was a problem connecting to the database")
-                print(f"\n{error}")
-            
+        redis_creds = {
+            "address": ("localhost", 6379)
+        }
+
+        try:
+            self.pool = await asyncpg.create_pool(**pool_creds)
+            self.redis = await aioredis.create_redis_pool(**redis_creds)
+        except Exception as e:
+            print("There was a problem")
+            print("\n" + str(e))
+
         with open("schema.sql", "r") as schema:
             await self.pool.execute(schema.read())
-
-        prefixes = await self.pool.fetch("SELECT * FROM prefixes")
-        self.prefixes = {}
-        for current_row in prefixes:
-            self.prefixes[current_row["guildid"]] = current_row["prefix"]
 
         self.default_prefixes = [
             "p.", "P.", "p!", "P!",
