e54155fff7c176e1e09dfad5f6f04396c89b9cff
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-05-03 17:48:19 -0400

    Added Congratulations

--- cogs/disstrack.py
+++ cogs/disstrack.py
@@ -1,4 +1,3 @@
-"""This cog was only made so that reloading cogs.general will NOT stop the disstrack from being played in VC"""
 import discord
 from discord.ext import commands
 
@@ -7,15 +6,14 @@ class Disstrack(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_without_command = True)
-    async def disstrack(self, ctx):
+    async def play_song(self, ctx, name: str = "lasagna.mp3"):
         if ctx.author.voice != None:
             try:
                 await ctx.author.voice.channel.connect()
             except discord.ClientException:
                 pass
 
-            source = discord.FFmpegPCMAudio("lasagna.mp3")
+            source = discord.FFmpegPCMAudio(name)
 
             try:
                 ctx.voice_client.play(source)
@@ -26,7 +24,6 @@ class Disstrack(commands.Cog):
         else:
             await ctx.send("You must be connected to a voice channel")
 
-    @disstrack.command(aliases = ["leave", "end", "disconnect"])
     async def stop(self, ctx):
         if ctx.voice_client != None:
             await ctx.voice_client.disconnect()
@@ -34,6 +31,21 @@ class Disstrack(commands.Cog):
         else:
             await ctx.send(f"{self.bot.user.name} is not currently in a voice channel")
 
+    @commands.group(invoke_without_command = True)
+    async def congrats(self, ctx):
+        await self.play_song(ctx, "congrats.mp3")
+
+    @congrats.command(name = "stop", aliases = ["leave", "end", "disconnect"])
+    async def congrats_stop(self, ctx):
+        await self.stop(ctx)
+
+    @commands.group(invoke_without_command = True)
+    async def disstrack(self, ctx):
+        await self.play_song(ctx)
+
+    @disstrack.command(name = "stop", aliases = ["leave", "end", "disconnect"])
+    async def disstrack_stop(self, ctx):
+        await self.stop(ctx)
 
 def setup(bot):
     bot.add_cog(Disstrack(bot))
