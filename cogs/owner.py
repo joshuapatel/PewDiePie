3be38dee2553eb7eee51bc135c7905bf1a89c13a
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-05-11 00:26:32 -0400

    Variable name spelling error fixed

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,7 +14,7 @@ import config # Note: Only importing config module since it's easier to use in e
 
 
 SUPPORT_GUILD = 499357399690379264
-SUPPOR_GUILD_AUTH_ROLE = 531176653184040961
+SUPPORT_GUILD_AUTH_ROLE = 531176653184040961
 
 class Owner(commands.Cog):
     def __init__(self, bot):
@@ -22,7 +22,7 @@ class Owner(commands.Cog):
 
     async def cmdauthcheck(ctx): # pylint: disable=E0213
         guild = ctx.bot.get_guild(SUPPORT_GUILD)
-        role = guild.get_role(SUPPOR_GUILD_AUTH_ROLE)
+        role = guild.get_role(SUPPORT_GUILD_AUTH_ROLE)
         user = guild.get_member(ctx.author.id) # pylint: disable=E1101
         try:
             if role in user.roles:
