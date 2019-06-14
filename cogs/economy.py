0f4ba282b15613bd79bbea45a593394073e82c68
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-06-14 01:02:58 -0400

    Removed Redis support, added the crime command, and improved the daily command

--- cogs/economy.py
+++ cogs/economy.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 # -> Miscellaneous
 import random
 import datetime
+import humanize
 # -> Loop
 import asyncio
 
@@ -28,35 +29,32 @@ class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bc_image = "<:bro_coin:541363630189576193>"
+        self._mc = set()
         self.bot.loop.create_task(self.cache())
 
     async def cache(self):
         users = await self.bot.pool.fetch("SELECT guildid, userid FROM econ")
-        for u in users:
-            await self.bot.redis.sadd("econ_users", f"{u[0]}:{u[1]}")
+        self._mc.add(f"{u[0]}:{u[1]}" for u in users)
 
+        # Shovel
         self.bot.econ["pos"] = await self.bot.pool.fetch("SELECT name, id FROM shovel WHERE fate = true")
         self.bot.econ["neg"] = await self.bot.pool.fetch("SELECT name, id FROM shovel WHERE fate = false")
+        # Crime
+        self.bot.econ["crime"] = {}
+        self.bot.econ["crime"]["pos"] = await self.bot.pool.fetch("SELECT name, id FROM crime WHERE fate = true")
+        self.bot.econ["crime"]["neg"] = await self.bot.pool.fetch("SELECT name, id FROM crime WHERE fate = false")
 
-    async def cad_user(ctx): # pylint: disable=no-self-argument
-        # pylint: disable=E1101
-        lock = asyncio.Lock(loop=ctx.bot.loop)
+    async def cog_before_invoke(self, ctx):
+        dc = f"{ctx.guild.id}:{ctx.author.id}" in self._mc
 
-        async with lock:
-            dc = await ctx.bot.redis.sismember("econ_users", f"{ctx.guild.id}:{ctx.author.id}")
-
-            if not dc:
-                await ctx.bot.pool.execute("INSERT INTO econ VALUES ($1, $2, $3)", 0, ctx.author.id, ctx.guild.id)
-                await ctx.bot.redis.sadd("econ_users", f"{ctx.guild.id}:{ctx.author.id}")
-                return True
-
-            return True
-        # pylint: enable=E1101
+        if not dc:
+            await self.bot.pool.execute("INSERT INTO econ VALUES ($1, $2, $3)", 0, ctx.author.id, ctx.guild.id)
+            self._mc.add(f"{ctx.guild.id}:{ctx.author.id}")
 
     @commands.command(aliases = ["shove;", "shove", "shv", "sh", "work"])
-    @commands.check(cad_user)
     @commands.cooldown(5, 10, commands.BucketType.member)
     async def shovel(self, ctx):
+        """You work all day shoveling for Bro Coins"""
         fate = random.choice([True, False, True, False, True])
 
         if fate:
@@ -85,24 +83,69 @@ class Economy(commands.Cog):
         await self.bot.pool.execute("UPDATE econ SET coins = coins + $1 WHERE userid = $2 AND guildid = $3", ctg, ctx.author.id, ctx.guild.id)
         await self.bot.pool.execute("UPDATE econ SET uses = uses + 1 WHERE userid = $1 AND guildid = $2", ctx.author.id, ctx.guild.id)
 
-    @commands.command(aliases = ["day", "dai;y"])
-    @commands.check(cad_user)
-    @commands.cooldown(1, 86400, commands.BucketType.member)
-    async def daily(self, ctx):
+    @commands.command()
+    @commands.cooldown(5, 10, commands.BucketType.member)
+    async def crime(self, ctx):
+        """You commit a crime and gain or lose coins based on your success"""
+        fate = random.choice([True, False, True, False, True, False, False])
 
-        ctg = 10000
+        if fate:
+            ctg = random.randint(1, 1500)
+            phrases = self.bot.econ["crime"]["pos"]
+        else:
+            ctg = -random.randint(1, 700)
+            phrases = self.bot.econ["crime"]["neg"]
 
-        em = discord.Embed(colour = discord.Colour.green())
-        em.add_field(name="Daily", value=f"You cashed in your daily bonus of {ctg:,d} {self.bc_image} brocoins!")
+        try:
+            phrases = random.choice(phrases)
+        except IndexError:
+            phrases = {"id": 1, "name": "You need {ctg} {tcoinimage} to add crime phrases."}
+
+        message = phrases["name"].replace("{ctg}", f"{ctg:,d}").replace("{tcoinimage}", self.bc_image)
+
+        if fate:
+            em = discord.Embed(color = discord.Color.green())
+        else:
+            em = discord.Embed(color = discord.Color.red())
+
+        em.add_field(name = "Crime", value = message)
+        em.set_footer(text = f"Phrase #{phrases['id']:,d}")
         await ctx.send(embed = em)
 
         await self.bot.pool.execute("UPDATE econ SET coins = coins + $1 WHERE userid = $2 AND guildid = $3", ctg, ctx.author.id, ctx.guild.id)
         await self.bot.pool.execute("UPDATE econ SET uses = uses + 1 WHERE userid = $1 AND guildid = $2", ctx.author.id, ctx.guild.id)
 
+    @commands.command()
+    async def daily(self, ctx):
+        """Daily bonus of Bro Coins"""
+        time = await self.bot.pool.fetchval("SELECT time FROM daily WHERE userid = $1 AND guildid = $2", ctx.author.id, ctx.guild.id)
+        now = datetime.datetime.utcnow()
+        tomorrow = now + datetime.timedelta(days=1)
+
+        if time is not None and time > now:
+            human_time = humanize.naturaldelta(time - now)
+
+            em = discord.Embed(color=discord.Color.dark_teal())
+            em.add_field(name="Daily Cooldown", value=f"You must wait {human_time} until you can cash in your daily bonus.")
+            await ctx.send(embed=em)
+            return
+
+        ctg = random.randint(4500, 10000)
+
+        em = discord.Embed(color=discord.Colour.green())
+        em.add_field(name="Daily", value=f"You cashed in your daily bonus of {ctg:,d} {self.bc_image}")
+        await ctx.send(embed=em)
+
+        if time is None:
+            await self.bot.pool.execute("INSERT INTO daily VALUES ($1, $2, $3)", ctx.author.id, ctx.guild.id, tomorrow)
+        else:
+            await self.bot.pool.execute("UPDATE daily SET time = $1 WHERE userid = $2 AND guildid = $3", tomorrow, ctx.author.id, ctx.guild.id)
+
+        await self.bot.pool.execute("UPDATE econ SET coins = coins + $1 WHERE userid = $2 AND guildid = $3", ctg, ctx.author.id, ctx.guild.id)
 
     @commands.command(aliases = ["give", "givemoney", "send", "sendmoney", "add", "addmoney"])
-    @commands.check(cad_user)
     async def pay(self, ctx, amount: AmountConverter, *, user: discord.Member):
+        """Pays a user with a specified amount of Bro Coins"""
         if 0 >= amount:
             em = discord.Embed(color = discord.Color.dark_teal())
             em.add_field(name = "Too Small", value = f"You cannot send {self.bc_image} that is 0 or smaller")
@@ -112,7 +155,7 @@ class Economy(commands.Cog):
         aucash = await self.bot.pool.fetchval("SELECT coins FROM econ WHERE userid = $1 AND guildid = $2", ctx.author.id, ctx.guild.id)
         if aucash >= amount:
             repcheck = await self.bot.pool.fetchrow("SELECT * FROM econ WHERE userid = $1 AND guildid = $2", user.id, ctx.guild.id)
-            if repcheck == None:
+            if repcheck is None:
                 await self.bot.pool.execute("INSERT INTO econ VALUES ($1, $2, $3)", 0, user.id, ctx.guild.id)
 
             await self.bot.pool.execute("UPDATE econ SET coins = coins + $1 WHERE userid = $2 AND guildid = $3", amount, user.id, ctx.guild.id)
@@ -129,32 +172,26 @@ class Economy(commands.Cog):
 
     @commands.command(aliases = ["bal", "money", "cash", "$", "coins", "coin", "bank"])
     async def balance(self, ctx, *, user: discord.Member = None):
-        if user != None:
-            uid = user
-        else:
-            uid = ctx.author
+        """Shows the users balance and economy uses"""
+        if user is None:
+            user = ctx.author
 
-        baluses = await self.bot.pool.fetchrow("SELECT coins, uses FROM econ WHERE userid = $1 AND guildid = $2", uid.id, ctx.guild.id)
-        if baluses == None:
-            bal = 0
-            uses = 0
-        else:
-            bal = baluses["coins"]
-            uses = baluses["uses"]
+        coins, uses = await self.bot.pool.fetchrow("SELECT coins, uses FROM econ WHERE userid = $1 AND guildid = $2", user.id, ctx.guild.id)
 
         if uses == 1:
             u = "use"
         else:
             u = "uses"
 
-        em = discord.Embed(color = discord.Color.blue())
-        em.set_author(name = f"{uid.name}#{uid.discriminator}", icon_url = uid.avatar_url)
-        em.add_field(name = "Bro Coins", value = f"{bal:,d} {self.bc_image}")
-        em.add_field(name = "Economy Uses", value = f"{uses:,d} {u}")
-        await ctx.send(embed = em)
+        em = discord.Embed(color=discord.Color.blue())
+        em.set_author(name=user, icon_url=user.avatar_url)
+        em.add_field(name="Bro Coins", value=f"{coins:,d} {self.bc_image}")
+        em.add_field(name="Economy Uses", value=f"{uses:,d} {u}")
+        await ctx.send(embed=em)
 
     @commands.command(aliases = ["lb", "lead", "board", "leadboard"])
     async def leaderboard(self, ctx, param: str = ""):
+        """Shows the leaderboard for Bro Coins"""
         if param.lower() in ["server", "guild"]:
             coins = await self.bot.pool.fetch("SELECT * FROM econ WHERE guildid = $1 ORDER BY coins DESC LIMIT 5", ctx.guild.id)
         else:
@@ -196,9 +233,9 @@ class Economy(commands.Cog):
         await ctx.send(embed = em)
 
     @commands.command(aliases = ["bet", "ontheline", "bets", "dice", "die"])
-    @commands.check(cad_user)
     @commands.cooldown(1, 60, commands.BucketType.member)
     async def gamble(self, ctx, amount: AmountConverter = 5000):
+        """Gambles a specific amount of Bro Coins"""
         usercoins = await self.bot.pool.fetchval("SELECT coins FROM econ WHERE userid = $1 AND guildid = $2", ctx.author.id, ctx.guild.id)
         if 0 >= amount:
             em = discord.Embed(color = discord.Color.dark_teal())
@@ -228,9 +265,9 @@ class Economy(commands.Cog):
             self.bot.get_command("gamble").reset_cooldown(ctx)
 
     @commands.command(aliases = ["rob", "take", "thief", "steel", "theft", "thieves"])
-    @commands.check(cad_user)
     @commands.cooldown(1, 7200, commands.BucketType.member)
     async def steal(self, ctx, *, user: discord.Member):
+        """Steals from a user"""
         if user.bot:
             em = discord.Embed(color = discord.Color.dark_teal())
             em.add_field(name = "Cannot Steal", value = "You cannot steal from bots")
@@ -244,7 +281,7 @@ class Economy(commands.Cog):
             return
 
         mu = await self.bot.pool.fetchval("SELECT coins FROM econ WHERE userid = $1 AND guildid = $2", user.id, ctx.guild.id)
-        if mu == None:
+        if mu is None:
             mu = 0
 
         coinchance = random.choice([True, False, True, True, False, False])
@@ -272,8 +309,8 @@ class Economy(commands.Cog):
             await ctx.send(embed = em)
 
     @commands.command()
-    @commands.check(cad_user)
     async def transfer(self, ctx, amount: AmountConverter, *, guild: str):
+        """Sends Bro Coins to another server. The max amount is 50% of your coins."""
         if 0 >= amount:
             em = discord.Embed(color = discord.Color.dark_teal())
             em.add_field(name = "Too Small", value = f"You cannot transfer {self.bc_image} that is 0 or smaller")
@@ -281,7 +318,7 @@ class Economy(commands.Cog):
             return
 
         guild = discord.utils.get(self.bot.guilds, name = guild)
-        if guild == None:
+        if guild is None:
             em = discord.Embed(color = discord.Color.dark_teal())
             em.add_field(name = "Guild Not Found", value = f"{self.bot.user.name} could not find the guild")
             await ctx.send(embed = em)
@@ -299,7 +336,7 @@ class Economy(commands.Cog):
 
         if coins >= amount:
             gc = await self.bot.pool.fetchrow("SELECT * FROM econ WHERE userid = $1 AND guildid = $2", ctx.author.id, guild.id)
-            if gc == None:
+            if gc is None:
                 await self.bot.pool.execute("INSERT INTO econ VALUES ($1, $2, $3)", amount, ctx.author.id, guild.id)
             else:
                 await self.bot.pool.execute("UPDATE econ SET coins = coins + $1 WHERE userid = $2 AND guildid = $3", amount, ctx.author.id, guild.id)
@@ -319,6 +356,7 @@ class Economy(commands.Cog):
 
     @commands.command(aliases = ["stats", "stat"])
     async def statistics(self, ctx):
+        """Shows statistics on Bro Coin usage"""
         econ_info = await self.bot.pool.fetchrow("SELECT COUNT(coins), AVG(coins), SUM(coins) FROM econ")
 
         tcusbcount = econ_info["count"]
@@ -337,7 +375,7 @@ class Economy(commands.Cog):
         ff = await self.bot.pool.fetchval("SELECT COUNT(*) FROM shovel WHERE fate = false")
 
         tluname = self.bot.get_user(tlu["userid"])
-        if tluname == None:
+        if tluname is None:
             tluname = "User Not Found"
 
         em = discord.Embed(color = discord.Color.red())
