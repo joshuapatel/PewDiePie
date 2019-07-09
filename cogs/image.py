7c1740f13f866fc4ba3e42fbce031de44a308c80
ItsWolfiePlaysMC <martingame665156@gmail.com>
2019-07-08 23:25:20 -0400

    Trying out image generation

---
+++ cogs/image.py
@@ -0,0 +1,28 @@
+# -> Discord
+import discord
+from discord.ext import commands
+
+import aiohttp
+
+
+class Image(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def changemymind(self, ctx, *, text: str):
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://nekobot.xyz/api/imagegen?type=changemymind&text={text}'.replace(" " "%20"), headers=headers) as r:
+                raw = await r.json()
+                imgurl = raw['message']
+                em = discord.Embed(title = "Change My Mind", color = discord.Color.red())
+                em.set_image(url=imgurl)
+                await ctx.send(embed = em)
+
+
+def setup(bot):
+    bot.add_cog(Image(bot))
